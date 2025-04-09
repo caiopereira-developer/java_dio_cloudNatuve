d770650605940c9f7e7cb3e0bfc5f6a83a7eab36
Caio <caio2pnascimento@gmail.com>
2025-04-08 21:53:47 -0300

    commit para anotação dos tipos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-    }
+         
+         double salarioMinimo = 2500;
+         short numeroCurto = 1;
+         int numeroNormal = numeroCurto;
+         short numeroCurto2 = (short)numeroNormal;
+         
+         int numero = 1;
+
+         numero = 10;
+
+         System.out.println(numero);
+        
+         // final define uma constante, toda connstante em letra maiuscula
+         final double VALOR_PI = 3.14;
+
+        }
 }
